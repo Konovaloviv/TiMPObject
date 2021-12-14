b8bdad5f234564d8dbb369a75a4c4451e78210e3
Konovaloviv <95358839+Konovaloviv@users.noreply.github.com>
2021-12-14 23:10:45 +0700

    Initial commit

---
+++ ConsoleApplication2.1/ConsoleApplication1/pch.h
@@ -0,0 +1,65 @@
+#ifndef PCH_H
+#define PCH_H
+#include <fstream>
+using namespace std;
+class transport {
+private:
+	int spead;
+	int distance;
+public:
+	static transport* In(ifstream &ifst);
+	virtual void InData(ifstream &ifst) = 0; // ввод
+	virtual void Out(ofstream &ofst) = 0; // вывод
+	void OutCommon(ofstream &ofst);
+};
+class container
+{
+private:
+	class Node {
+	public:
+		Node();
+		Node* Next;
+		Node* Prev;
+		transport* data;
+	};
+	Node* Top;
+	int count;
+public:
+	void In(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	void Clear(); // очистка контейнера от фигур
+	container(); // инициализация контейнера
+	~container() { Clear(); }
+};
+
+
+class plane : public transport {
+	int range;
+	int c;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	plane() {} // создание без инициализации.
+};
+class traine : public transport {
+	int count;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	traine() {} // создание без инициализации.
+};
+
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H#pragma once
+enum type_of_ship { LINER, TUG, TANKER };
+struct ship : public transport {
+	int water_displacement;
+	type_of_ship type;
+public:
+	void InData(ifstream &ifst);
+	void Out(ofstream &ofst);
+	ship() {}
+};
---
+++ ConsoleApplication2.2/ConsoleApplication1/pch.h
@@ -0,0 +1,57 @@
+#ifndef PCH_H
+#define PCH_H
+#include <fstream>
+using namespace std;
+class transport {
+private:
+	int spead;
+	int distance;
+public:
+	static transport* In(ifstream &ifst);
+	virtual void InData(ifstream &ifst) = 0; // ввод
+	virtual void Out(ofstream &ofst) = 0; // вывод
+	float Travel_time();
+	void OutCommon(ofstream &ofst);
+};
+class container
+{
+private:
+	class Node {
+	public:
+		Node();
+		Node* Next;
+		Node* Prev;
+		transport* data;
+	};
+	Node* Top;
+	int count;
+public:
+	void In(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	void Clear(); // очистка контейнера от фигур
+	container(); // инициализация контейнера
+	~container() { Clear(); }
+};
+
+
+class plane : public transport {
+	int range;
+	int c;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	plane() {} // создание без инициализации.
+};
+class traine : public transport {
+	int count;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	traine() {} // создание без инициализации.
+};
+
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H#pragma once
